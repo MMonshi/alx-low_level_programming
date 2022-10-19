f430f65acc78fc6801ce42cf8393f7585d3757e8
MMonshi <monsheila.n@gmail.com>
2022-10-19 03:02:01 -0700

    breakthru

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,25 @@
-#idndef main_h
-#define main_h
-extern int_putchar(char c);
-extern void print_alphabet(void);
-extern void print_alphabet_x10(void);
-extern int_islower(int c);
-extern int_isalpha(int c);
-extern int print_sign(int n);
-extern int_abs(int);
-extern int print_last_digit(int);
-extern void jack_bauer(void);
-extern void times_table(void);
-extern int add(int, int);
-extern void print_to_98(int n);
-extern void print_times_table(int n);
-#endif
+int_putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int_islower(int c);
+
+int_isalpha(int c);
+
+int print_sign(int n);
+
+int_abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
